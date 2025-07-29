aebc16cc0820e2f3f82cc0ea81c1821ebbe08236
Tejasree22179 <gondralatejasree@gmail.com>
2025-07-29 22:49:31 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,43 @@
+'''NOTE: programs are commented in triple quotes for easy indivial testing
+uncomment the quotes to run it'''
+#create a list  of 5 integers
+'''list1=list(map(int,input().split(' ')))
+print(list1)
+n=len(list1)
+#print all elemnets using a loop
+for i in range(n):
+    print(list1[i])
+#sum of array elements
+sum1=0
+for i in range(n):
+    sum1+=list1[i]
+print(sum1)
+#insert element at the end
+list1.append(788)
+print(list1)
+#pop the last element
+list1.pop()
+print(list1)
+#max and minimum
+for i in range(n):
+    for j in range(i+1,n):
+        if list1[i]>list1[j]:
+            pass'''
+#print(list1[i],"it is maximum")
+#min
+'''for i in range(n):
+    for j in range(i+1,n):
+        if list1[i]<list1[j]:
+            pass
+print(list1[i],"it is minimum")'''
+'''print(max(list1))
+print(min(list1))
+print(list1.count(5))'''
+# reverse order
+'''list1=[3,4,5,6,7,8]
+n=len(list1)
+for i in range(n-1,-1,-1):
+    print(list1[i],end=",")'''
+
+
+        
